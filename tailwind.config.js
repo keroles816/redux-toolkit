536355c59a392c0a001bc8547fb575808ca89466
CodeSandbox <support@codesandbox.io>
2025-08-15 13:58:07 +0000

    Sandbox snapshot commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      fontWeight: {
+        thin: "100",
+        extraLight: "200",
+        light: "300",
+        normal: "400",
+        medium: "500",
+        semiBold: "600",
+        bold: "700",
+        extraBold: "800",
+        black: "900",
+      },
+    },
   },
   plugins: [],
 };
