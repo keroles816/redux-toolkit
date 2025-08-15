4433baf32eff42ecb662b3f052d1a97d66b5ca70
CodeSandbox <support@codesandbox.io>
2025-08-15 14:59:33 +0000

    Sandbox snapshot commit

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,33 @@
+import MeubelLogo from "../assets/Icons/Meubel House_Logos-05.png";
+import { Link } from "react-router-dom";
+const NavBar = () => {
+  return (
+    <nav className="flex flex-row items-center justify-between py-8 px-6">
+      <div className="flex justify-center items-center gap-2">
+        <img src={MeubelLogo} alt="home logo" />
+        <h3 className="font-bold text-4xl font-montserrat">Furniro</h3>
+      </div>
+
+      <ul
+        className="flex justify-center items-center  gap-[75px] font-poppins 
+      font-semibold 
+      "
+      >
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/shop">Shop</Link>
+        </li>
+        <li>
+          <Link to="/About">About</Link>
+        </li>
+        <li>
+          <Link to="/Contat">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,9 @@
 import React from "react";
-import MeubelLogo from "../assets/Icons/Meubel House_Logos-05.png";
+import NavBar from "../components/NavBar.tsx"
 const Home = () => {
   return (
     <div className=" min-w-full h-full">
-      <div className="flex flex-row items-center justify-between py-8 px-6">
-        <div className="flex justify-center items-center gap-2">
-          <img src={MeubelLogo} alt="home logo" />
-          <h3 className="font-bold text-4xl font-montserrat">Furniro</h3>
-        </div>
-      </div>
+      <NavBar/>
     </div>
   );
 };
