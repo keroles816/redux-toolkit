c7d64c7c8d2a447e39eccdd4e4a68f4f1ba5e5a8
CodeSandbox <support@codesandbox.io>
2025-08-17 13:09:23 +0000

    Sandbox snapshot commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,16 @@
-import React from "react";
-import NavBar from "../components/NavBar.tsx"
+import NavBar from "../components/NavBar.tsx";
+import backgroundHomeImage from "../assets/photos/backgroundHomeImage.png";
 const Home = () => {
   return (
     <div className=" min-w-full h-full">
-      <NavBar/>
+      <NavBar />
+
+      <section className="min-w-full">
+        <div
+          className="bg-cover bg-center h-screen"
+          style={{ backgroundImage: `url(${backgroundHomeImage})` }}
+        ></div>
+      </section>
     </div>
   );
 };
