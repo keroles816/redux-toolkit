678774983ea3ccb3c73860a24d39ed41146f696c
keroles abdelmessih <79661118+keroles816@users.noreply.github.com>
2025-08-17 10:12:50 +0000

    finsish navbare

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,9 @@
 import MeubelLogo from "../assets/Icons/Meubel House_Logos-05.png";
 import { Link } from "react-router-dom";
+import Account from "../assets/Icons/Account.png";
+import love from "../assets/Icons/love.png";
+import search from "../assets/Icons/search.png";
+import shopingcart from "../assets/Icons/shopingcart.png";
 const NavBar = () => {
   return (
     <nav className="flex flex-row items-center justify-between py-8 px-6">
@@ -28,21 +32,29 @@ const NavBar = () => {
       </ul>
 
       <ul
-        className="flex justify-center items-center  gap-[75px] font-poppins 
-      font-semibold 
+        className="flex justify-center items-center  gap-6
+    
       "
       >
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/">
+            <img src={Account} alt="Account" />
+          </Link>
         </li>
         <li>
-          <Link to="/shop">Shop</Link>
+          <Link to="/shop">
+            <img src={love} alt="love" />
+          </Link>
         </li>
         <li>
-          <Link to="/About">p</Link>
+          <Link to="/About">
+            <img src={search} alt="search" />
+          </Link>
         </li>
         <li>
-          <Link to="/Contat">Contact</Link>
+          <Link to="/Contat">
+            <img src={shopingcart} alt="shopingcart" />
+          </Link>
         </li>
       </ul>
     </nav>
