087e3adaf4c854184f5565500e474ee804724dca
CodeSandbox <support@codesandbox.io>
2025-08-19 12:04:53 +0000

    Sandbox snapshot commit

--- src/features/furniture/furnitureReducer.js
+++ src/features/furniture/furnitureReducer.js
@@ -1,12 +1,21 @@
 import { initialState } from "./intialState";
 
+const furnitureReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TO_THE_CART:
+      const item = action.payload;
+      const avalibality = state.cart.map((i) => i.id === item.id);
 
-const furnitureReducer = (state = initialState , action) => {
-    switch(action.type){
-        case    :
-            return{
-
-            }
-    }
-
-}
+      if (avalibality) {
+        //if is avaliable i need to take the object and add to cart with quantity.
+        return {
+          ...state,
+          cart: state.cart.map((i) =>
+            i.id === item.id ? { ...i, quantity: i.quantity + 1 } : i
+          ),
+        };
+      } else {
+        return {};
+      }
+  }
+};
